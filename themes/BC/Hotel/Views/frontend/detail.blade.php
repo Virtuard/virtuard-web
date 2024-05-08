2e7c3adb6c81ec95c15a86bd67e4f2d606fe2da2
Yulius D Hipa <yuliusdhipa@gmail.com>
2024-05-08 10:41:41 +0800

    fix: login popup when guest click book

--- themes/BC/Hotel/Views/frontend/detail.blade.php
+++ themes/BC/Hotel/Views/frontend/detail.blade.php
@@ -81,7 +81,7 @@
     <script type="text/javascript" src="{{ asset("libs/sticky/jquery.sticky.js") }}"></script>
     <script type="text/javascript" src="{{ asset('module/hotel/js/single-hotel.js?_ver='.config('app.asset_version')) }}"></script>
 
-    <script src="{{ asset('libs/ipanorama/src/lib/jquery.min.js') }}"></script>
+    {{-- <script src="{{ asset('libs/ipanorama/src/lib/jquery.min.js') }}"></script> --}}
     <script src="{{ asset('libs/ipanorama/src/jquery.ipanorama.js') }}"></script>
     <script src="{{ asset('libs/ipanorama/src/lib/three.min.js') }}"></script>
     <script src="{{ asset('libs/ipanorama/src/main.js') }}"></script>
