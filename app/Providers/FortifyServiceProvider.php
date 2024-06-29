758befaf4ff6651a5f29268b62eeed125927f45c
Yulius D Hipa <yuliusdhipa@gmail.com>
2024-06-29 18:51:30 +0800

    validation login user reset

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -67,7 +67,7 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::authenticateUsing(function (Request $request) {
             $user = User::where('email', $request->email)->first();
 
-            if ($user && $user->status == "publish" && $user->need_update_pw == '2') {
+            if ($user && $user->need_update_pw == '2') {
                 abort(403);
             }
 
