73d6118192729d375c91c1f079009bb74b725981
Yulius D Hipa <yuliusdhipa@gmail.com>
2024-07-17 12:35:31 +0800

    ref: referral button show for guest

--- resources/views/partials/listing/copy-referral-script.blade.php
+++ resources/views/partials/listing/copy-referral-script.blade.php
@@ -7,11 +7,9 @@
         const url = "{{ $row->getDetailUrl() }}";
         const isLoggedIn = "{{ auth()->check() ? 'true' : 'false' }}";
         const userName = "{{ auth()->user()->user_name ?? 'null' }}";
-        const isSubscribed = "true";
         
         document.getElementById('copyReferralButton').addEventListener('click', function() {
-            console.log(isLoggedIn)
-            if (isLoggedIn && isSubscribed) {
+            if (isLoggedIn == 'true') {
                 let referralUrl = `${url}?reference=${userName}`;
 
                 //Copy the referral URL to the clipboard
@@ -28,7 +26,7 @@
                 });
 
             } else {
-                // Show an error message if the user is not logged in or not subscribed
+                $('#login').modal('show');
             }
         });
     }
