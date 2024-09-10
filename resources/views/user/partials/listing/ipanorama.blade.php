c1ab4204e8e16e8cccfc20359a14ceda4f2c8cff
Yulius D Hipa <yuliusdhipa@gmail.com>
2024-09-10 16:02:27 +0800

    update wording preview low quality

--- resources/views/user/partials/listing/ipanorama.blade.php
+++ resources/views/user/partials/listing/ipanorama.blade.php
@@ -6,6 +6,7 @@
     <div class="pull-right mb-3">
         <a href="{{ route("$row->type.detail", ['slug' => $row->slug, 'preview_panorama' => '1']) }}"
             class="btn btn-warning btn-sm">
+            <i class="fa fa-info-circle"></i>
             Click to View Low Quality (suggested for iphone)
         </a>
     </div>
