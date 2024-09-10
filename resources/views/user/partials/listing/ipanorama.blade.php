3aa26513cda589e3f173d072b5626cabd380cc70
Yulius D Hipa <yuliusdhipa@gmail.com>
2024-09-10 16:01:28 +0800

    update wording preview low quality

--- resources/views/user/partials/listing/ipanorama.blade.php
+++ resources/views/user/partials/listing/ipanorama.blade.php
@@ -6,9 +6,7 @@
     <div class="pull-right mb-3">
         <a href="{{ route("$row->type.detail", ['slug' => $row->slug, 'preview_panorama' => '1']) }}"
             class="btn btn-warning btn-sm">
-            <i class="fa fa-info-circle" data-toggle="tooltip" data-placement="top"
-                title="If the panorama preview is not visible, please try using low mode"></i>
-            Click to View Low Quality
+            Click to View Low Quality (suggested for iphone)
         </a>
     </div>
 @endif
