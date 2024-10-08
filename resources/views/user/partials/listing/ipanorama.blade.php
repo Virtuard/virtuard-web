a4703685d4f31736b08525b3e7679e5f8b9d4506
Yulius D Hipa <yuliusdhipa@gmail.com>
2024-10-08 10:31:30 +0800

    fix row ipanorama

--- resources/views/user/partials/listing/ipanorama.blade.php
+++ resources/views/user/partials/listing/ipanorama.blade.php
@@ -3,12 +3,16 @@
         data-user_id="{{ $row->ipanorama->user_id }}">
 
     <div id="mypanorama" class="mypanorama-preview"></div>
-    <div class="pull-right mb-3">
-        <a href="{{ route("$row->type.detail", ['slug' => $row->slug, 'preview_panorama' => '1']) }}"
-            class="btn btn-warning btn-sm">
-            <i class="fa fa-apple" data-toggle="tooltip" data-placement="top"
-                title="If the screen is blank, click here to view low quality"></i> 
-            Iphone view
-        </a>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="pull-right mb-3">
+                <a href="{{ route("$row->type.detail", ['slug' => $row->slug, 'preview_panorama' => '1']) }}"
+                    class="btn btn-warning btn-sm">
+                    <i class="fa fa-apple" data-toggle="tooltip" data-placement="top"
+                        title="If the screen is blank, click here to view low quality"></i> 
+                    Iphone view
+                </a>
+            </div>
+        </div>
     </div>
 @endif
