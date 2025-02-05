cd464bfe308d2febd6aa8dbb216d34ff60de3271
DanDev_ <danuart21@gmail.com>
2025-02-05 16:10:13 +0800

    feat: update meta description

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Explore 3D & 360 Virtual Tours | Virtual Tours by Virtuard</title>
 
-    <meta name="description" content="Explore 3D & 360 Virtual Tours | Virtual Tours by Virtuard">
+    <meta name="description" content="Want to showcase your property, hotel, restaurant, or shop like never before? With Virtuard, you can create your listing and 360° Virtual Tour on your own – at no cost and with no assistance needed!">
     {{-- <meta name="keywords" content="keyword1, keyword2, keyword3"> --}}
     {{-- <meta name="author" content="Your Name or Company"> --}}
 
