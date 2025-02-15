aa512fb4e5b85030d23b4a20f2d1d43b2c585f73
DanDev_ <danuart21@gmail.com>
2025-02-15 16:15:56 +0800

    feat: update height carousel and the responsive

--- themes/Base/Template/Views/frontend/blocks/form-search-all-service/index.blade.php
+++ themes/Base/Template/Views/frontend/blocks/form-search-all-service/index.blade.php
@@ -1,4 +1,18 @@
-<div class="bravo-form-search-all {{$style}} @if(!empty($style) and $style == "carousel") bravo-form-search-slider @endif" @if(empty($style)) style="height: 100vh; background-image: linear-gradient(0deg,rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)),url('{{$bg_image_url}}') !important" @endif>
+@push('css')
+    <style>
+        .bravo-form-search-all {
+            height: 80vh;
+        }
+
+        @media(max-width: 768px) {
+            .bravo-form-search-all {
+                height: 120vh;
+            }
+        }
+    </style>    
+@endpush
+
+<div class="bravo-form-search-all {{$style}} @if(!empty($style) and $style == "carousel") bravo-form-search-slider @endif" @if(empty($style)) style="background-image: linear-gradient(0deg,rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)),url('{{$bg_image_url}}') !important" @endif>
     @if(in_array($style,['carousel','']))
         @include("Template::frontend.blocks.form-search-all-service.style-normal")
     @endif
