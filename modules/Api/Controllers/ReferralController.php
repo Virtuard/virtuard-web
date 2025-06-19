2dc9c025b410c9fde4d374f2d057263d5263ea59
Danar Cahyadi <danarcahyadi21@gmail.com>
2025-06-19 12:39:32 +0800

    feat(referral): Implement referral report API for consuming in mobile.

---
+++ modules/Api/Controllers/ReferralController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Modules\Api\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Modules\Booking\Models\Booking;
+use Modules\Booking\Models\BookingPayment;
+
+class ReferralController extends Controller
+{
+    protected $bookingClass;
+    protected $bookingPayment;
+    public function __construct()
+    {
+        $this->bookingClass = Booking::class;
+        $this->bookingPayment = BookingPayment::class;
+    }
+
+    public function getReports(Request $request) {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }   
+
+        if ($user->role_id == 2) {
+            $bookings = $this->bookingPayment::getReferralHistoryAPI(Auth::id())->paginate(10);
+        } 
+        $data = [
+            'referals'    => !empty($bookings) ? $bookings : [],
+            'statues'     => config('booking.statuses'),
+            'userInfo'    => [
+                'username' => $user->user_name,
+                'userId'   => $user->id,
+            ],
+
+        ];
+
+
+        return response()->json([
+            'success' => 1,
+            'data'  => $data,
+        ], 200);
+
+
+
+
+    }
+
+}
--- modules/Booking/Models/BookingPayment.php
+++ modules/Booking/Models/BookingPayment.php
@@ -9,6 +9,9 @@ use Modules\User\Models\Plan;
 class BookingPayment extends BaseModel
 {
     protected $table = 'bravo_user_plan';
+    protected $casts = [
+        'amount' => 'float', // 👈 ensures amount is returned as a float
+    ];
 
 
     public static function getReferralHistory($userId)
@@ -17,7 +20,18 @@ class BookingPayment extends BaseModel
         ->where('status', 1)
         ->orderBy('created_at', 'desc');
     }
-
+    
+    
+    // This function only for API `referral report`
+    public static function getReferralHistoryAPI($userId)
+    {
+        return self::where('user_id', $userId)
+            ->join("users", "users.id", "=", "bravo_user_plan.referal_user_id")
+            ->join("bravo_plans","bravo_plans.id", "=", "bravo_user_plan.plan_id")
+            ->select("bravo_user_plan.id AS referral_id", "bravo_user_plan.referal_amount AS amount", "bravo_user_plan.created_at AS created_at", "users.name as user_name", "bravo_plans.title as plan_name")
+            ->where('bravo_user_plan.status', 1)
+            ->orderBy('bravo_user_plan.created_at', 'desc');
+    }
     public static function getReferralAdminHistory()
     {
         return self::query()
