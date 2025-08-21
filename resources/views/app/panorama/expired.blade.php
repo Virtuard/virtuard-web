b33ac18a1132368dfcacca31e76f9adb2942f74d
Yulius D Hipa <yuliusdhipa@gmail.com>
2025-08-21 23:00:02 +0800

    ref: page plan expired

--- resources/views/app/panorama/expired.blade.php
+++ resources/views/app/panorama/expired.blade.php
@@ -6,9 +6,9 @@
             <div class="container">
                 <div class="row my-2">
                     <div class="col-md-12 text-center expired-plan">
-                        <h2>Subscription Expired</h2>
-                        <p>Your subscription has expired. Please upgrade to continue using the service.</p>
-                        <a href="{{ route('user.plan') }}" class="btn btn-primary">Upgrade Subscription</a>
+                        <h2>{{__('Subscription Expired')}}</h2>
+                        <p class="py-3">{{__('This virtual tour has expired. If you is owner of this virtual tour, please upgrade your subscription to continue using the service.')}}</p>
+                        <a href="{{ route('user.plan') }}" class="btn btn-primary">{{__('Upgrade Subscription')}}</a>
                     </div>
                 </div>
             </div>
