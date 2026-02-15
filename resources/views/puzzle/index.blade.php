8e0bbe6123abb3aee20d49a39918aa3bb8057258
Yulius D Hipa <yuliusdhipa@gmail.com>
2026-02-15 18:13:48 +0800

    feat: dynamic params puzzle

--- resources/views/puzzle/index.blade.php
+++ resources/views/puzzle/index.blade.php
@@ -5,19 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }}</title>
 
-    <link rel="icon" href="https://virtuard.com/uploads/0000/1/2026/02/14/logodefinitivo.png" type="image/x-icon">
+    <link rel="icon" href="{{$imgUrl}}" type="image/x-icon">
     @if($isSharingMode && $imgUrl)
     <!-- Open Graph for Social Previews (only for sharing mode) -->
     <meta property="og:title" content="{{ $title }}" />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="https://virtuard.com/uploads/0000/1/2026/02/14/logodefinitivo1.png" />
+    <meta property="og:image" content="{{$imgUrl}}" />
     <meta property="og:description" content="Can you solve this puzzle? Click to play!" />
     <meta property="og:url" content="{{ url()->current() }}" />
     
     <!-- Twitter Card -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:title" content="{{ $title }}">
-    <meta name="twitter:image" content="https://virtuard.com/uploads/0000/1/2026/02/14/logodefinitivo1.png">
+    <meta name="twitter:image" content="{{$imgUrl}}">
     <meta name="twitter:description" content="Can you solve this puzzle? Click to play!">
     @endif
     
